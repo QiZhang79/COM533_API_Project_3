4f47fae5b1dc4e63faf10619e930916f001057cb
Qi Zhang <zhangqitydx@gmail.com>
2017-11-27 15:10:30 -0600

    Update

--- step3/server.js
+++ step3/server.js
@@ -28,6 +28,30 @@ app.post('/', function (req, res) {
         res.render('index', {weather: null, error: 'Invalid city name, please try again'});
       } else {
         let weatherText = `It's ${weather.main.temp} degrees and ${weather.weather[0].main} in ${weather.name}!`;
+        // let weatherIcon = `${weather.weather[0].icon}`;
+        // console.log(weatherIcon);
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
+});
+
+
+app.post('/', function (req, res) {
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}` //If you use Celsius you’d add: units=metric and if you use Fahrenheit you’d use units=imperial.
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Error, please try again'});
+    } else {
+      let weather = JSON.parse(body);
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Invalid city name, please try again'});
+      } else {
+        let weatherText = `It's ${weather.main.temp} degrees and ${weather.weather[0].main} in ${weather.name}!`;
+        let weatherIcon = `${weather.weather[0].icon}`;
+        console.log(weatherIcon);
         res.render('index', {weather: weatherText, error: null});
       }
     }
