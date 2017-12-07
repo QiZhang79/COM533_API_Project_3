84d676dee7a0700e8e55cea28e0715da6bef6309
Qi Zhang <zhangqitydx@gmail.com>
2017-12-07 14:25:28 -0600

    Add if statement to decide using which api

--- step4/server.js
+++ step4/server.js
@@ -17,55 +17,51 @@ app.get('/', function (req, res) {
   res.render('index.ejs', {weather: null, error: null});
 })
 
-app.post('/city', function (req, res) {
-  let city = req.body.searchWeather;
-  console.log(city);
-  //If you use Celsius you’d add: units=metric and if you use Fahrenheit you’d use units=imperial.
-  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}` 
-
-  request(url, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
-    } else {
-      let weather = JSON.parse(body);
-      if(weather.main == undefined){
-        res.render('index', {weather: null, error: 'Invalid city name, please try again'});
-      } else {
-        weather.weatherMainText = `${weather.weather[0].main}`;
-        weather.weatherTempText = `${weather.main.temp} degrees`;
-        weather.weatherIcon = `https://process.filestackapi.com/AhTgLagciQByzXpFGRI0Az/resize=width:120/http://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
-        console.log(weather.weatherIcon);
-
-        res.render('index', {weather: weather, error: null});
-      }
-    }
-  });
-});
-
-
-app.post('/zipcode', function (req, res) {
-  let zip = req.body.searchWeather;
-  console.log('zip');
-  let zipurl = `http://api.openweathermap.org/data/2.5/forecast?zip=${zip}&units=imperial&appid=${apiKey}`;
-  console.log('zipurl');
-  request(zipurl, function (err, response, body) {
-    if(err){
-      res.render('index', {weather: null, error: 'Error, please try again'});
-      res.json({ message: 'post created!' });
-    } else {
-      let weather = JSON.parse(body);
-      if(weather.list[0].main == undefined){
-        res.render('index', {weather: null, error: 'Invalid zipcode, please try again'});
+app.post('/', function (req, res) {
+  let input = req.body.searchWeather;
+  a = input[0];
+  if (a >= '0' && a <= '9') {
+    let zipurl = `http://api.openweathermap.org/data/2.5/forecast?zip=${input}&units=imperial&appid=${apiKey}`;
+    request(zipurl, function (err, response, body) {
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
         res.json({ message: 'post created!' });
       } else {
-        console.log(weather.list[0].main.temp); 
-        weather.weatherText = `It's ${weather.list[0].main.temp} degrees.`;
-        weather.weatherIcon = `http://openweathermap.org/img/w/${weather.list[0].weather[0].icon}.png`;
-        res.render('index', {weather: weather, error: null});
+        let weather = JSON.parse(body);
+        if(weather.list[0].main == undefined){
+          res.render('index', {weather: null, error: 'Invalid zipcode, please try again'});
+          res.json({ message: 'post created!' });
+        } else {
+          console.log(weather.list[0].main.temp); 
+          weather.weatherMainText = `${weather.list[0].weather[0].main}`;
+          weather.weatherTempText = `${weather.list[0].main.temp} degrees.`;
+          weather.weatherIcon = `https://process.filestackapi.com/AhTgLagciQByzXpFGRI0Az/resize=width:120/http://openweathermap.org/img/w/${weather.list[0].weather[0].icon}.png`;
+          res.render('index', {weather: weather, error: null});
+        }
       }
-    }
-  });
-})
+    });
+
+  } else {
+    let url = `http://api.openweathermap.org/data/2.5/weather?q=${input}&units=imperial&appid=${apiKey}`
+    request(url, function (err, response, body) {
+      if(err){
+        res.render('index', {weather: null, error: 'Error, please try again'});
+      } else {
+        let weather = JSON.parse(body);
+        if(weather.main == undefined){
+          res.render('index', {weather: null, error: 'Invalid city name, please try again'});
+        } else {
+          weather.weatherMainText = `${weather.weather[0].main}`;
+          weather.weatherTempText = `${weather.main.temp} degrees`;
+          weather.weatherIcon = `https://process.filestackapi.com/AhTgLagciQByzXpFGRI0Az/resize=width:120/http://openweathermap.org/img/w/${weather.weather[0].icon}.png`;
+
+          res.render('index', {weather: weather, error: null});
+        }
+      }
+    });
+  }
+
+});
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
